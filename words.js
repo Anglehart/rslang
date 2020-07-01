f9630019ce6e3484f4598fddbdae43b9be4537f6
Nikita <niko07335@gmail.com>
2020-07-01 14:23:11 +0300

    init: initial commit

---
+++ words.js
@@ -0,0 +1,38 @@
+
+function checkWord(id){
+  // проверка слова на его нахождение в базе
+
+  // return boolean
+}
+
+function checkDifficulty(id){
+  // проверка сложности слова
+
+  // retrun int
+}
+
+
+function delet(id) {
+  // задаем слову difficult: -1 
+  
+  // return "Done!"
+}
+
+function restore(id) {
+  // задаем слову difficult: 2(?)
+  
+  // return "Done!"
+}
+
+function upDifficulty(id) {
+ // изменяем текущий 'difficult' на '3'
+ 
+  // return "Done!"
+}
+
+function downDifficulty(id) {
+  // отнимаетм от текущего значения 'difficult'  1
+  // если 0, то оставляем 0
+
+  // return "Done!"
+}
