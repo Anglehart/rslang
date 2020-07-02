12ed55de3389037a92698d70ed46c46bdb5b2de6
Nikita <niko07335@gmail.com>
2020-07-02 23:29:34 +0300

    feat: add test data for localStorage

--- words.js
+++ words.js
@@ -1,36 +1,103 @@
 
-function checkWord(id){
-  // проверка слова на его нахождение в базе
 
+function setLocalStorage() {
+  localStorage.setItem('email', 'team17@mail.ru');
+  localStorage.setItem('password', 'RsSchool2020!');
+  localStorage.setItem('userId', '5eefa4639896e10017eea40c');
+  localStorage.setItem('token', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVlZWZhNDYzOTg5NmUxMDAxN2VlYTQwYyIsImlhdCI6MTU5MzcxNDI5MywiZXhwIjoxNTkzNzI4NjkzfQ.ZAd9yGal3i9wAJkxa0os-8X0h0A0QFIi2QuFw6Jfo6U');
+}
+setLocalStorage();
+
+function getEmail() {
+  const email = localStorage.getItem('email');
+  return email;
+}
+function getUserId() {
+  const userId = localStorage.getItem('userId');
+  return userId;
+}
+function getToken() {
+  const token = localStorage.getItem('token');
+  return token;
+}
+
+async function addWord(wordId) {
+  const url = `https://afternoon-falls-25894.herokuapp.com/users/${getUserId()}/words/${wordId}`;
+  fetch(url, {
+    method: 'POST',
+    headers: {
+      'Authorization': `Bearer ${getToken()}`,
+      Accept: 'application/json',
+      'Content-Type': 'applications/json',
+    },
+  }).then((data) => {
+    console.log(data);
+  })
+
+}
+
+
+async function getWords() {
+  const url = `https://afternoon-falls-25894.herokuapp.com/users/${getUserId()}/words/`
+  fetch(url, {
+    method: 'GET',
+    headers: {
+      'Authorization': `Bearer ${getToken()}`,
+      Accept: 'application/json',
+      'Content-Type': 'applications/json',
+    },
+  }).then((data) => {
+    console.log(data);
+  })
+  // return
+}
+
+
+// проверка слова на его нахождение в базе
+async function checkWord(wordId) {
+  const url = `https://afternoon-falls-25894.herokuapp.com/users/${getUserId()}/words/${wordId}`
+  fetch(url, {
+    method: 'GET',
+    headers: {
+      'Authorization': `Bearer ${getToken()}`,
+      Accept: 'application/json',
+      'Content-Type': 'applications/json',
+    },
+  }).then((data) => {
+    console.log(data);
+  })
   // return boolean
 }
 
-function checkDifficulty(id){
+addWord('5e9f5ee35eb9e72bc21af4a1');
+getWords();
+
+function checkDifficulty(wordId) {
   // проверка сложности слова
 
   // retrun int
 }
 
 
-function delet(id) {
+function delet(wordId) {
   // задаем слову difficult: -1 
-  
+
   // return "Done!"
 }
 
-function restore(id) {
+function restore(wordId) {
   // задаем слову difficult: 2(?)
-  
+
   // return "Done!"
 }
 
-function upDifficulty(id) {
- // изменяем текущий 'difficult' на '3'
- 
+function upDifficulty(wordId) {
+  // изменяем текущий 'difficult' на '3'
+
   // return "Done!"
 }
 
-function downDifficulty(id) {
+function downDifficulty(wordId) {
   // отнимаетм от текущего значения 'difficult'  1
   // если 0, то оставляем 0
 
