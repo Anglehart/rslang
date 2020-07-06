84d7383b4d50a137b2fc9f16192bcfa40d56102f
Boris Kobzarev <planez@bk.ru>
2020-07-06 16:28:46 +0300

    fix: delete temporary code

--- settings/index.js
+++ settings/index.js
@@ -1,9 +1,4 @@
 window.onload = function() {
-  // Временное внесение авторищационных данных
-  localStorage.setItem('userId', '5eefa4639896e10017eea40c');
-  localStorage.setItem('token', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVlZWZhNDYzOTg5NmUxMDAxN2VlYTQwYyIsImlhdCI6MTU5MzkzNTc0OCwiZXhwIjoxNTkzOTUwMTQ4fQ.8kZmR_7pppLklYNmXOlgn1Mr8upYwIsD9WYNRxIU7UM');
-  // Конец
-  
   loadSettings();
 }
 
@@ -96,35 +91,3 @@ function showMessage(boolean) {
   Message.classList.add('showMessage');
   setTimeout(() => { Message.classList.remove('showMessage') }, 3000);
 }
-
-// Эту функцию нужно запустить сразу после регистрации пользователя. 
-// Передать Вике для добавления.
-/*
-function setBasicSettings() {
-  const userId = localStorage.getItem('userId');
-  const token = localStorage.getItem('token');
-  const data = {
-    "wordsPerDay": 20,
-    "optional": {
-      "cardsPerDay": 20,
-      "showTranslation": true,
-      "showTextMeaning": true,
-      "showTextExample": true,
-      "showAnswerButton": true,
-      "showDeleteButton": true,
-      "showDifficultWordsButton": true,
-      "showWordsStatusButtons": true
-    }
-  }
-    
-  fetch(`https://afternoon-falls-25894.herokuapp.com/users/${userId}/settings`, {
-    method: 'PUT',
-    headers: {
-      Accept: 'application/json',
-      'Content-Type': 'application/json',
-      Authorization: `Bearer ${token}`,
-    },
-    body: JSON.stringify(data),
-  })
-}
-*/
