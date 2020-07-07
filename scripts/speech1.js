7af7cacd59ca1bf947e7aa3e0a1dc7c8a0ac4234
DaryaDan <56804467+DaryaDan@users.noreply.github.com>
2020-07-07 21:05:14 +0300

    Update and rename slova1.js to speech1.js

--- scripts/speech1.js
+++ scripts/speech1.js
@@ -1,20 +1,20 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable no-undef */
-function slova1(data, item, imageUrl, mini, zzz) {
+function speech1(data, item, imageUrl, mini, syllable) {
   if (data.wordTranslate === item.textContent) {
     changeImage(imageUrl);
-    document.querySelector(`#${zzz}`).insertAdjacentHTML('afterBegin', '<img class = "correct" alt="correct" src ="img/correct.png"></img>');
+    document.querySelector(`#${syllable}`).insertAdjacentHTML('afterBegin', '<img class = "correct" alt="correct" src ="img/correct.png"></img>');
     document.querySelector('#words').style.pointerEvents = 'none';
     document.querySelector('.photo').style.display = 'block';
     document.querySelector('.obj').style.display = 'block';
     document.querySelector('.sound').style.display = 'none';
     document.querySelector('.dontKnow').classList.add('hide');
     document.querySelector('.next').classList.remove('hide');
-    document.querySelector('.little-sound').innerHTML = '<img class = "little" alt="little" src ="img/zvuk.png"></img>';
+    document.querySelector('.little-sound').innerHTML = '<img class = "little" alt="little" src ="img/sound.png"></img>';
     document.querySelector('.word').innerHTML = `<p>${data.word}</p>`;
     const contin1 = document.querySelector('.next');
     contin1.onclick = function nextclick() {
-      schet();
+      score();
       contin();
     };
     if (sessionStorage.truefalse !== '1') {
@@ -26,7 +26,7 @@ function slova1(data, item, imageUrl, mini, zzz) {
     }
   } else if (data.wordTranslate !== item.textContent) {
     sessionStorage.truefalse = 1;
-    document.querySelector(`#${zzz}`).style.textDecoration = 'line-through';
-    document.querySelector(`#${zzz}`).style.pointerEvents = 'none';
+    document.querySelector(`#${syllable}`).style.textDecoration = 'line-through';
+    document.querySelector(`#${syllable}`).style.pointerEvents = 'none';
   }
 }
