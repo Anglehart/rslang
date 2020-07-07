facda23e47b2ec80794fc3fee352a4be017eedab
DaryaDan <56804467+DaryaDan@users.noreply.github.com>
2020-07-07 21:06:55 +0300

    changed variable names

--- scripts/dontknow.js
+++ scripts/dontknow.js
@@ -4,19 +4,19 @@ const next = document.querySelector('.dontKnow');
 next.addEventListener('click', () => {
   document.querySelectorAll('.cell').forEach((el) => el.style.textDecoration = 'none');
   document.querySelectorAll('.cell').forEach((el) => el.style.pointerEvents = '');
-  schet();
+  score();
   repeat();
 });
 
 document.addEventListener('keypress', (event) => {
   if (event.keyCode === 13) {
-    const obj1 = document.getElementById('igra');
+    const obj1 = document.getElementById('acting');
     const display = window.getComputedStyle(obj1, null).getPropertyValue('display');
     if (display === 'flex') {
       if (document.querySelector('.dontKnow').classList.contains('hide') === false) {
         document.querySelectorAll('.cell').forEach((el) => el.style.textDecoration = 'none');
         document.querySelectorAll('.cell').forEach((el) => el.style.pointerEvents = '');
-        schet();
+        score();
         repeat();
       }
     }
