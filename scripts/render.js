db17e27d62debc9c2b38aeb5d8fe05f65b6047ab
DaryaDan <56804467+DaryaDan@users.noreply.github.com>
2020-07-07 21:09:25 +0300

    changed variable names

--- scripts/render.js
+++ scripts/render.js
@@ -5,7 +5,7 @@ function render(data) {
   document.querySelector('.poster').style.opacity = '1';
   document.querySelector('.poster').style.boxShadow = '0 0 30px rgba(0,0,0,0.6)';
   aud.play();
-  aud.onended = function konets() {
+  aud.onended = function ending() {
     document.querySelector('.poster').style.opacity = '.6';
     document.querySelector('.poster').style.boxShadow = 'none';
   };
@@ -17,13 +17,13 @@ function renderForSmall(data) {
   document.querySelector('.little').style.opacity = '1';
   document.querySelector('.little').style.boxShadow = '0 0 30px rgba(0,0,0,0.6)';
   aud.play();
-  aud.onended = function littlekonets() {
+  aud.onended = function littleEnding() {
     document.querySelector('.little').style.opacity = '.6';
     document.querySelector('.little').style.boxShadow = 'none';
   };
 }
 
-function renderZvuk(data) {
+function renderSound(data) {
   const audioUrl = `${this.dataLink}${data}`;
   const aud = new Audio(`${audioUrl}`);
   aud.play();
