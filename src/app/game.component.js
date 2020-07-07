ea2092f633a4bb88ef8d352f7e809f944d53835f
Anglehart <planez@bk.ru>
2020-07-07 11:59:56 +0300

    feat: add network part

--- src/app/game.component.js
+++ src/app/game.component.js
@@ -1,17 +1,38 @@
 import { Component } from './core.component';
 import overlay from './overlay.component';
+import network from './network.service';
 
 
 class Game extends Component {
   constructor(config) {
     super(config);
+    this.currentRound = 0;
+  }
 
+  initComponent(){
+    this.startNewGame();
+  }
+
+  startNewGame() {
+    this.currentRound = 1;
+    this.startNewRound();
+  }
+
+  async startNewRound() {
+    let a = await network.getDataForRound(this.currentRound);
+    this.drawQuestion(a);
+    this.currentRound += 1;
+    console.log(a);
+  }
+
+  drawQuestion(a) {
+    document.querySelector('.question').innerHTML = a[4];
   }
 
 }
 
 const game = new Game({
   selector: '.game-wrapper',
-  template: '',
+  template: '<div class="question"></div>',
 });
 export default game;
--- src/app/network.service.js
+++ src/app/network.service.js
@@ -1,7 +1,71 @@
 //import storageService from './storage.service';
 
 class NetworkService {
+  async getDataForRound(round) {
+    let group = 0;
+    if (round < 4) {
+      group = 0;
+    } else if (round >= 4 && round < 7) {
+      group = 1;
+    } else if (round >= 7 && round < 10) {
+      group = 2;
+    } else if (round == 10 || round == 11) {
+      group = 3;
+    } else if (round === 12 || round === 13) {
+      group = 4;
+    } else if (round === 14 || round === 15) {
+      group = 5;
+    }
+    const wordArray = await this.getWords(group);
+    const finalArr = await this.wordsAPI(wordArray);
+    return finalArr;
+  }
 
+  async wordsAPI(array) {
+    const definition = array[0].word;
+    return fetch(`https://wordsapiv1.p.rapidapi.com/words/${definition}/definitions`, {
+    	'method': 'GET',
+    	'headers': {
+    		'x-rapidapi-host': 'wordsapiv1.p.rapidapi.com',
+    		'x-rapidapi-key': 'a2e3a10c09msh3a5cbdc010f793fp1b3879jsnaf4c8929aac8'
+    	}
+    })
+    .then((response) => response.json())
+    .then(response => {
+    	array.push(response.definitions[0].definition);
+      return array;
+    })
+    .catch(err => {
+    	console.log(err);
+    });
+  }
+
+  async getWords(group) {
+    const page = this.randomInteger(0, 29);
+    const url = `https://afternoon-falls-25894.herokuapp.com/words?page=${page}&group=${group}`;
+    return fetch(url)
+    .then((response) => {
+      if (response.ok) return response;
+    })
+    .then((response) => response.json())
+    .then((data) => {
+      let array = [];
+      for (let i = 0; i < 4; i += 1) {
+        const word = data[this.randomInteger(0, 19)];
+        if (!array.includes(word)) {
+          array.push(word);
+        } else {
+          i -= 1;
+        }
+      }
+      return array;
+    })
+  }
+
+  randomInteger(min, max) {
+    const rand = min - 0.5 + Math.random() * (max - min + 1);
+    return Math.round(rand);
+  }
 }
 
 const networkService = new NetworkService();
