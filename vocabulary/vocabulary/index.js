5867d3d0a60fd772005a513109172c05dab85771
Nikita <niko07335@gmail.com>
2020-07-09 10:04:54 +0300

    move files for PR

--- vocabulary/vocabulary/index.js
+++ vocabulary/vocabulary/index.js
@@ -13,13 +13,6 @@ async function start() {
   const allWords = await wordLibrary.getWords();
   allWords.forEach((element) => wordLibrary.getWordData(element));
   console.log(await wordLibrary.getWords());
-
-  await wordLibrary.updateWordTest('5e9f5ee35eb9e72bc21af716', '0', 1);
-  await wordLibrary.updateWordTest('5e9f5ee45eb9e72bc21b020e', '0', 1);
-  await wordLibrary.updateWordTest('5e9f5ee35eb9e72bc21af4b4', '0', 1 * 2);
-  await wordLibrary.updateWordTest('5e9f5ee35eb9e72bc21af4b5', '0', 1 * 3);
-  await wordLibrary.updateWordTest('5e9f5ee35eb9e72bc21af4b6', '0', 1 * 3);
-  await wordLibrary.updateWordTest('5e9f5ee35eb9e72bc21af4b7', '0', 1 * 3);
 }
 start();
 
