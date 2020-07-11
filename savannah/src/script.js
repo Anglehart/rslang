b8c7c1cfaac066b90ee6754cab1e11b42b3f1b64
Vicktoryia Kiyanko <57707166+Mikisum@users.noreply.github.com>
2020-07-11 13:48:58 +0300

    Savannah (#14)
    * feat: add generation html
    
    * feat: add checking answers
    
    * feat: add project config
    
    * feat: add game statistics
    
    * feat: add moving background
    
    * feat: add voice acting
    
    * feat: add keystroke handler
    
    * feat: add game update
    
    * feat: add level selection
    
    * fix: fix styles for question word
    
    * refactor: change structure of project
    
    * feat: add playing audio for word
    
    * fix: fix result statistic
    
    * fix: fix animation
    
    * fix:fix style
    
    * refactor: refactor code
    
    * refactor: delete file
    
    * refactor: move readme file
    
    Co-authored-by: unknown <mikisum@mail.ru[>

---
+++ savannah/src/script.js
@@ -0,0 +1,27 @@
+import Savannah from './Savannah.js';
+// const mainPage = document.getElementById('mainPage');
+// const startButton = document.getElementById('startButton');
+// const gamePage = document.getElementById('gamePage'); 
+// const answer = document.getElementById('answer');
+
+
+// window.onload = function () {
+//   document.getElementById('container').classList.add('active');
+// };
+
+
+const savannah = new Savannah();
+document.body.append(savannah.getContainer());
+// document.body.addEventListener('load', () => {
+//   console.log(1);
+// });
+
+// document.body.addEventListener('load', () => {
+//   console.log(2);
+// });
+
+// document.body.addEventListener('load', () => {
+//   console.log(3);
+// });
+
+
